ad9c5c3ba1125717b8046619ba5f9fa1e7d21684
Greyson Daugherty <sgreysond@gmail.com>
2025-09-04 17:17:56 -0400

    feat: Add comprehensive build and test scripts
    - Add test-all.sh for running all project tests
    - Add build-all.sh for building all projects
    - Update package configurations
    - All builds successful locally

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,14 @@
 module.exports = {
+  transform: {
+    '^.+\\.(t|j)sx?$': ['@swc/jest', {
+      jsc: { target: 'es2020', parser: { syntax: 'typescript', tsx: false } },
+      module: { type: 'commonjs' }
+    }]
+  },
+  testEnvironment: 'node',
+  testTimeout: 15000
+};
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
